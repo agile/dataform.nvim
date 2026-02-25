7d229ba4b5da249497cfd41177c822d587f43b06
Mike Vincent <mikevincent@simpli.fi>
2026-02-24 20:16:43 -0600

    ReOrg: clean up test setup

--- Makefile
+++ Makefile
@@ -1,5 +1,19 @@
-test:
+DEPS_PATH = tests/.deps
+
+test: deps
 	nvim --headless --clean -u tests/init.lua -c "lua MiniTest.run()" -c "qa!"
 
-test_file:
+test_file: deps
 	nvim --headless --clean -u tests/init.lua -c "lua MiniTest.run_file('$(FILE)')" -c "qa!"
+
+deps: $(DEPS_PATH)/mini.nvim $(DEPS_PATH)/plenary.nvim
+
+$(DEPS_PATH)/mini.nvim:
+	@mkdir -p $(DEPS_PATH)
+	git clone --filter=blob:none https://github.com/echasnovski/mini.nvim $@
+
+$(DEPS_PATH)/plenary.nvim:
+	@mkdir -p $(DEPS_PATH)
+	git clone --filter=blob:none https://github.com/nvim-lua/plenary.nvim $@
+
+.PHONY: test test_file deps
--- tests/init.lua
+++ tests/init.lua
@@ -1,26 +1,12 @@
 local plugin_root = vim.fn.getcwd()
 vim.opt.runtimepath:append(plugin_root)
 
--- Set up 'mini.test'
+-- Add dependencies to runtimepath
 local deps_path = plugin_root .. '/tests/.deps'
-if vim.fn.isdirectory(deps_path) == 0 then
-  vim.fn.mkdir(deps_path, 'p')
-end
+vim.opt.runtimepath:append(deps_path .. '/mini.nvim')
+vim.opt.runtimepath:append(deps_path .. '/plenary.nvim')
 
-local minipath = deps_path .. '/mini.nvim'
-if vim.fn.isdirectory(minipath) == 0 then
-  print('Downloading mini.nvim...')
-  vim.fn.system({ 'git', 'clone', '--filter=blob:none', 'https://github.com/echasnovski/mini.nvim', minipath })
-end
-vim.opt.runtimepath:append(minipath)
-
-local plenarypath = deps_path .. '/plenary.nvim'
-if vim.fn.isdirectory(plenarypath) == 0 then
-  print('Downloading plenary.nvim...')
-  vim.fn.system({ 'git', 'clone', '--filter=blob:none', 'https://github.com/nvim-lua/plenary.nvim', plenarypath })
-end
-vim.opt.runtimepath:append(plenarypath)
-
+-- Set up 'mini.test'
 require('mini.test').setup({
   execute = {
     reporter = require('mini.test').gen_reporter.stdout(),
